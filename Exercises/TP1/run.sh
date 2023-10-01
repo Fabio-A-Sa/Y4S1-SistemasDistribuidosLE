c54e5764874c4cb72296e39b9e18cdbbd723a4db
Fábio Sá <up202007658@edu.fe.up.pt>
2023-10-01 11:41:08 +0100

    exercises: TP1 run update

--- Exercises/TP1/run.sh
+++ Exercises/TP1/run.sh
@@ -1,3 +1,4 @@
+export MACOSX_DEPLOYMENT_TARGET=13.0
 rm *.o
 gcc -o client.o client.c -I/opt/homebrew/include -L/opt/homebrew/lib -lzmq
 gcc -o server.o server.c -I/opt/homebrew/include -L/opt/homebrew/lib -lzmq
